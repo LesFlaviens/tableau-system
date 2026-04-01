39153691546b4842ce819356a071a4d6fb74e0ae
LesFlaviens <iche.flavien@ichef.ch>
2026-04-02 00:31:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const DB_FILE = path.join(__dirname, 'database.json');
 app.get('/get-current-state', (req, res) => {
     fs.readFile(DB_FILE, 'utf8', (err, data) => {
         if (err) return res.json({ activeOrders: {} });
-        res.json(JSON.parse(data || '{"activeOrders": {}}'));
+        try {
+            res.json(JSON.parse(data || '{"activeOrders": {}}'));
+        } catch (e) {
+            res.json({ activeOrders: {} });
+        }
     });
 });
 
@@ -20,9 +24,12 @@ app.post('/update-order', (req, res) => {
     const { tableId, order } = req.body;
     fs.readFile(DB_FILE, 'utf8', (err, data) => {
         let db = { activeOrders: {} };
-        if (!err && data) db = JSON.parse(data);
+        if (!err && data) {
+            try { db = JSON.parse(data); } catch(e) {}
+        }
         if (order === null) delete db.activeOrders[tableId];
         else db.activeOrders[tableId] = order;
+
         fs.writeFile(DB_FILE, JSON.stringify(db, null, 2), (err) => {
             if (err) return res.status(500).send("Erreur");
             res.send("OK");
@@ -30,4 +37,4 @@ app.post('/update-order', (req, res) => {
     });
 });
 
-app.listen(port, () => console.log(`Serveur Empire Actif sur ${port}`));
+app.listen(port, () => console.log(`Empire Actif sur ${port}`));
