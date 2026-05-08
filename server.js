c32305c29645eb44cc8e766f19d2aace5fd34d4f
LesFlaviens <iche.flavien@ichef.ch>
2026-05-09 00:48:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 // 🛡️ CONFIGURATION STRIPE
 // RAPPEL: Remplacer 'sk_test_dummy_key' par ta vraie clé secrète Stripe (sk_test_...) plus tard !
-const stripeKey = process.env.STRIPE_SECRET_KEY || 'price_1TUmLhQ9Dw3nOFa4A6vHAW8i';
+const stripeKey = process.env.STRIPE_SECRET_KEY || 'price_1TN8NPQ9Dw3nOFa4jBaO1Gib';
 const stripe = require('stripe')(stripeKey);
 
 const app = express();
