d319b3de5fe4d76975ad3f3430cb9d25466d2d13
LesFlaviens <iche.flavien@ichef.ch>
2026-07-14 14:51:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const stripe = require('stripe')(stripeKey);
 const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
-const NUMERO_FLAVIEN = '+33641437265'; // Cible des alertes critiques
+const NUMERO_FLAVIEN = '+330641437265'; // Cible des alertes critiques
 
 let twilioClient = null;
 
